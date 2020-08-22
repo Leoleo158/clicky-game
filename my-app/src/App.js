c4f6f7c7a55524661f7aa86102c42d48192ce7f7
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-08-22 16:18:56 -0600

    adding the ability to import

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { Component } from 'react';
+import GridMDC from "./components/GridMDC";
+import PaperMDC from "./components/PaperMDC";
+import CharCard from "./components/CharCard";
+import Score from "./components/Score";
+import Alert from "./components/Alert";
+import NavBar from "./components/NavBar";
+import BottomNavMDC from "./components/BottomNavMDC";
+import characters from "./characters.json";
