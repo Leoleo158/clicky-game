4446e49ba109b944b34dea2a5264e84b601abba7
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-08-22 16:38:54 -0600

    rendering and export

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -68,3 +68,62 @@ class App extends Component {
       this.setState(this.state = newState)
     }
   }
+
+  render() {
+    return (
+      <div>
+        <NavBar style={{ background: "#313133", marginBottom: "5px" }} />
+
+        <GridMDC container direction="column" style={{ margin: "0 auto", maxWidth: 945 }}>
+
+          <GridMDC item lg={12}>
+            <PaperMDC>
+              {this.state.alertMessage === "GOOD CHOICE!" ? (
+                <Alert message={this.state.alertMessage} style={{ color: "green" }} />
+              ) : (
+                  <Alert message={this.state.alertMessage} style={{ color: "red" }} />
+                )}
+            </PaperMDC>
+          </GridMDC>
+
+          <GridMDC container justify="space-between">
+
+            <GridMDC item lg={6} md={6} sm={12} xs={12}>
+              <PaperMDC>
+                <Score type="Score" score={this.state.pickedChars.length} />
+              </PaperMDC>
+            </GridMDC>
+
+            <GridMDC item lg={6} md={6} sm={12} xs={12}>
+              <PaperMDC>
+                <Score type="Top Score" score={this.state.topScore} />
+              </PaperMDC>
+            </GridMDC>
+
+          </GridMDC>
+
+        </GridMDC>
+
+        <GridMDC container spacing={24} justify="center" style={{ maxWidth: 945, margin: "0 auto" }}>
+          {this.state.characters.map(char => (
+            <GridMDC item lg={3} md={3} sm={4} xs={6}>
+            <CharCard
+              id={char.id}
+              name={char.name}
+              image={char.image}
+              key={char.id}
+              handlePicked={this.handlePicked}
+            />
+            </GridMDC>
+          ))}
+        </GridMDC>
+        <BottomNavMDC style={{ background: "#313133", marginTop: "17.5px", paddingTop: "15px", borderTop: "2.5px solid slategray" }}>
+          <a href="https://github.com/Leoleo158/clicky-game" target="_blank" className="link" alt="clicky-game-github-link"><i className="fa fa-github fa-2x"></i></a>
+        </BottomNavMDC>
+
+      </div>
+    )
+  }
+}
+
+export default App;
